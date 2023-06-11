ded2d4ce023dcb4cc4425e0bdfa587b458cb079e
EMonHdez <133249327+EMonHdez@users.noreply.github.com>
2023-06-10 21:18:51 -0600

    Update Iteration Basic Examples.py

--- Iteration Basic Examples.py
+++ Iteration Basic Examples.py
@@ -1,10 +1,10 @@
-def DigitCounter(number): # This function will count how many digits does the given number has.
+def DigitCounter(number):# This function will count how many digits does the given number has.
     result=0
     while number!=0:
         number//=10
         result+=1
     return result
 
-def Delete_Odds(number_list):#This function will delete every odd number in the list
+def Delete_Odds(number_list):# This function will delete every odd number in the list
     number_list=[i for i in number_list if i%2==0]
     return number_list
